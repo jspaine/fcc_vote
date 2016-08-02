8f49215f0a836838a066c58fc19050af055ed414
jspaine <painejs@hotmail.com>
2016-08-02 12:59:46 +0200

    compose auth checks

--- server/lib/authService.js
+++ server/lib/authService.js
@@ -1,12 +1,30 @@
+import compose from 'koa-compose'
+
 export default {
-  isAuthenticated(ctx, next) {
-    if (ctx.state.user) return next()
-    else ctx.status = 401
-  },
-  hasRole(ctx, next) {
-    return (role) => {
+  isAuthenticated,
+  hasRole
+}
+
+/**
+ * Check if a user is authenticated
+ * @return {boolean}
+ */
+function isAuthenticated(ctx, next) {
+  if (ctx.state.user) return next()
+  else ctx.status = 401
+}
+
+/**
+ * Check if a user is authenticated and has required role
+ * @param {string} role the role to test for
+ * @return {boolean}
+ */
+function hasRole(role) {
+  return compose([
+    isAuthenticated, 
+    (ctx, next) => {
       if (ctx.state.user.role === role) return next()
       else ctx.status = 403
     }
-  }
-} 
+  ])
+}
