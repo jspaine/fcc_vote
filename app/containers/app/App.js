00c21b355283966db3077afd76d92e428984ccca
jspaine <painejs@hotmail.com>
2016-09-09 10:50:59 +0200

    dont import from react-toolbox root

--- app/containers/app/App.js
+++ app/containers/app/App.js
@@ -1,7 +1,7 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 import {push} from 'react-router-redux'
-import {Layout, Panel} from 'react-toolbox'
+import {Layout, Panel} from 'react-toolbox/lib/layout'
 
 import {openDrawer} from 'store/modules/ui'
 
